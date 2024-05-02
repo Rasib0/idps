2ed970653d27e527ab8319117bb6dcaa0aa15c25
Rasib0 <rasibnadeem101@gmail.com>
2024-05-02 23:24:35 +0500

    added example output

--- main.py
+++ main.py
@@ -10,51 +10,58 @@ def file_exists(file_path):
     return os.path.exists(file_path)
 
 
-def in_dir_sign(dir: str, encryption: str):
+def make_tags(dir: str, encryption: str):
     dir_list = os.listdir(dir)
     for file in dir_list:
         if not file.startswith("."):
             file_path = os.path.join(dir, file)
-            print(f"Signing {file}")
             subprocess.run(["mpirun", "-np", PROCESS_NUM, "pmac0.out","sign", file_path, encryption])
+            print(f"{file}.tag created")
 
-def in_dir_remove_tags(dir: str):
+def remove_tags(dir: str):
     for file in os.listdir(dir):
         if file.endswith(".tag"):
             file_path = os.path.join(dir, file)
-            print(f"Removing {file}")
             os.remove(file_path)
+            print(f"{file} removed")
 
 
-def in_dir_verify(dir: str, encryption: str) -> bool:
+def is_intruder_in_dir(dir: str, encryption: str) -> bool:
     for file in os.listdir(dir):
         if not file.endswith(".tag"):
             file_path = os.path.join(dir, file)
-            print(f"Verifing {file}")
+            print(f"Verifing {file} and {file}.tag")
             process = subprocess.run(["mpirun", "-np", PROCESS_NUM, "pmac0.out","verify", file_path, encryption, file_path + ".tag"], capture_output=True, text=True)
 
             if process.returncode == 0:
                 stdout = process.stdout.strip()
                 if stdout == "Tag is NOT valid":
                     print("Intruder Detected in file: ", file_path)
-                    return False
+                    return True
             else:
                 print("Something went wrong! Error code: ", process.returncode)
-                return True
-    return True
+                return False
+    return False 
+
+
+# ---- MAIN ----
+
 # compile pmac0.c if it does not exist
 if not file_exists("pmac0.out"):
     subprocess.run(["mpicc", "pmac0/pmac0.c", "-o", "pmac0.out"])
 
-in_dir_remove_tags("files")
+remove_tags("files")
+print("------------------------------")
 # Make the signature
-in_dir_sign("files", ENCYRPTION)
+make_tags("files", ENCYRPTION)
+print("------------------------------")
 
 # Verify the signatures for each file in loop
-check_num = 0
+check_num = 1
 while True:
     print(f"Check number: {check_num}")
-    if not in_dir_verify("files", ENCYRPTION):
+    if is_intruder_in_dir("files", ENCYRPTION):
         break
     time.sleep(0.5)
     check_num += 1
+    print("------------------------------")
